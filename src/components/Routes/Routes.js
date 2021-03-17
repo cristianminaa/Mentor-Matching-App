b83d0a156b9e43aff4a465a4643be673f9da71fa
Danail <tdanail12@gmail.com>
2021-03-17 17:10:38 +0200

    Project Structure

---
+++ src/components/Routes/Routes.js
@@ -0,0 +1,5 @@
+import React from "react"
+import "./styles.scss"
+
+const Routes = () => <>Routes</>
+export default Routes
---
+++ src/screens/Login/Login.js
@@ -0,0 +1,5 @@
+import React from "react"
+import "./styles.scss"
+
+const Login = () => <>Login</>
+export default Login
