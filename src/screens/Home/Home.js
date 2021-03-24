0106777be1d101e647dea7ba7359d2e63ed4cf11
patej744 <patej744@outlook.com>
2021-03-24 00:07:26 +0000

    added extra section

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -14,11 +14,11 @@ const Home = () => {
 
   return (
     <>
-      <div class="wrapper">
-        <div>
+      <div className="wrapper">
+        <div className="profilePic">
           <img src="https://bootdey.com/img/Content/avatar/avatar7.png" style={{height: 100, width:100}} alt="User-Profile-Image"/>
         </div>
-        <div class="searchbar">
+        <div className="searchbar">
           <img class="search-icon" src="https://static.thenounproject.com/png/101791-200.png" />
           <input placeholder="Search" type="text" class="search" />
         </div>
@@ -26,7 +26,19 @@ const Home = () => {
           <img height="30" src="https://www.flaticon.com/svg/vstatic/svg/1370/1370907.svg?token=exp=1616539867~hmac=9908b51de53e904e39c0e4456d40d79c" alt="Messenger free icon"/>
         </div>
       </div>
-      {displayUser(currentUser)}
+      <div className="profile">
+        <h3>Personal Details</h3>
+        {displayUser(currentUser)}
+      </div>
+      <div className="profile">
+        <p><h3>Catagories</h3></p>
+        <p>Tag A</p>
+        <p>Tag B</p>
+        <p>Tag C</p>
+        <p>Tag D</p>
+        <p>Tag E</p>
+        <p>Tag F</p>
+      </div>
     </>
   )
 }
