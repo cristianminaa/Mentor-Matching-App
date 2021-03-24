65d4d29b73334112b0e2b2bbfd7c5f58dd73b518
Cristian-Daniel Mina <c.mina@se18.qmul.ac.uk>
2021-03-24 19:38:44 +0200

    Fixed messenger icon

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -16,14 +16,14 @@ const Home = () => {
     <>
       <div className="wrapper">
         <div className="profilePic">
-          <img src="https://bootdey.com/img/Content/avatar/avatar7.png" style={{height: 100, width:100}} alt="User-Profile-Image"/>
+          <img src="https://bootdey.com/img/Content/avatar/avatar7.png" style={{height: 100, width:100}} alt="User-Profile"/>
         </div>
         <div className="searchbar">
-          <img class="search-icon" src="https://static.thenounproject.com/png/101791-200.png" />
+          <img class="search-icon" src="https://static.thenounproject.com/png/101791-200.png" alt="" />
           <input placeholder="Search" type="text" class="search" />
         </div>
         <div className="msg">
-          <img height="30" src="https://www.flaticon.com/svg/vstatic/svg/1370/1370907.svg?token=exp=1616539867~hmac=9908b51de53e904e39c0e4456d40d79c" alt="Messenger free icon"/>
+          <img height="30" src="http://cdn.onlinewebfonts.com/svg/img_397748.png" alt="Messenger free icon"/>
         </div>
       </div>
       <div className="profile">
@@ -31,7 +31,7 @@ const Home = () => {
         {displayUser(currentUser)}
       </div>
       <div className="profile">
-        <p><h3>Catagories</h3></p>
+        <p><h3>Categories</h3></p>
         <p>Tag A</p>
         <p>Tag B</p>
         <p>Tag C</p>
