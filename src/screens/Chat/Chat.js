7e3ee95b1d6e2bd03b58d9f09c8b4f1fa87e4379
Danail <tdanail12@gmail.com>
2021-04-04 22:14:39 +0300

    chat example

--- src/screens/Chat/Chat.js
+++ src/screens/Chat/Chat.js
@@ -1,29 +1,52 @@
 import React from "react"
-import Textarea from "../../components/Inputs/Textarea/Textarea"
-import Button from "../../components/Inputs/Button/Button"
+import { Inputs } from "../../components"
 import Conversation from "./conversation"
 import "./styles.scss"
 
+const Chat = () => {
+  const [chat, setChat] = React.useState([])
+  const [value, setValue] = React.useState("")
 
-const Chat = () =>{
-
-    return(
-        <div className="containerChat">
-            <div className="conversationsMenuLeft">
-                <p>conversationsMenuLeft</p>
-                <Conversation/>
-                </div>
-            <nav className="navbarTop"><p>navbarTop</p></nav>
-            <div className="main"><p>main</p></div>
-            <div className="barBottom">
-                <p>barBottom</p>
-                <Textarea className="textArea"/>
-                <Button className="buttonSend" text="send"></Button>
-                </div>
-            <footer>hello footer</footer>
-
-        </div>
-    );
+  const handleChat = () => {
+    if (value === "Hello Jay") {
+      setTimeout(() => {  setChat((chat) => [...chat,  { senderIsNotMe: true, message: "Hello Danail" },])}, 1000)
+    }
+    setChat([...chat, { message: value }])
+    setValue("")
+  }
+  return (
+    <div className="containerChat">
+      <div className="conversationsMenuLeft">
+        <p>conversationsMenuLeft</p>
+        <Conversation />
+      </div>
+      <nav className="navbarTop">
+        <p>navbarTop</p>
+      </nav>
+      <div className="main">
+        {chat?.map(({ message, senderIsNotMe }, i) => (
+          <p key={`message-${i}`} className={senderIsNotMe ? "right" : "left"}>
+            {message}
+          </p>
+        ))}
+      </div>
+      <div className="barBottom">
+        <p>barBottom</p>
+        <Inputs.Text
+          className="textArea"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          onKeyDown={({ keyCode }) => keyCode === 13 && handleChat()}
+        />
+        <Inputs.Button
+          className="buttonSend"
+          text="send"
+          onClick={() => handleChat()}
+        />
+      </div>
+      <footer>hello footer</footer>
+    </div>
+  )
 }
 
 export default Chat
