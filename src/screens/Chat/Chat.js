d2f5f181749b7fc12d83f6b1c2b6792dc0109ecc
Denis Grigoryev <d.grigoryev@se19.qmul.ac.uk>
2021-04-08 00:30:36 +0100

    chat modified

--- src/screens/Chat/Chat.js
+++ src/screens/Chat/Chat.js
@@ -15,25 +15,28 @@ const Chat = () => {
     setValue("")
   }
   return (
+    <>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous"></link>
+    
     <div className="containerChat">
-      <div className="conversationsMenuLeft">
-        <p>conversationsMenuLeft</p>
+      <nav className="navbarTop">
+          <p>navbarTop</p>
+        </nav>
+        <div className="inner d-flex justify-content-center">
+      <div className="conversationsMenuLeft border border-dark">
+        <h6 className="text-white bg-primary">Conversations</h6>
         <Conversation />
       </div>
-      <nav className="navbarTop">
-        <p>navbarTop</p>
-        <hr></hr>
-      </nav>
       
-      <div className="main">
+      <div className="main border border-dark">
         {chat?.map(({ message, senderIsNotMe }, i) => (
           <p key={`message-${i}`} className={senderIsNotMe ? "right" : "left"}>
             {message}
           </p>
         ))}
       </div>
-      <div className="barBottom">
-        <p>barBottom</p>
+      <center className="barBottom bg-secondary">
+        <p></p>
         <Inputs.Text
           className="textArea"
           value={value}
@@ -45,9 +48,12 @@ const Chat = () => {
           text="send"
           onClick={() => handleChat()}
         />
-      </div>
+      </center>
+
+  </div>
       <footer>hello footer</footer>
     </div>
+    </>
   )
 }
 
--- src/screens/Chat/conversation.js
+++ src/screens/Chat/conversation.js
@@ -8,10 +8,14 @@ const converation = () =>{
 
 
     return(
-        <div className="containerConversation">
+        <>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous"></link>
+    
+        <div className="containerConversation border border-primary align-middle">
             <img src={avatar} alt="avatar"/>
             <p>Name Name</p>
         </div>
+        </>
     );
 }
 export default converation;
