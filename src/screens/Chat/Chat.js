a07f8016aa2cf77c0a59e8d9df9d089221fb046f
patej744 <patej744@outlook.com>
2021-04-14 18:02:01 +0100

    chat wrapper fixed and error message fixed for register

--- src/screens/Chat/Chat.js
+++ src/screens/Chat/Chat.js
@@ -57,26 +57,25 @@ const Chat = () => {
         crossorigin="anonymous"
       ></link>
       <div className="wrapper">
-        <div className="profilePic">
-          <NavLink to="/">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="50"
-              fill="ghostwhite"
-              class="bi bi-arrow-left-square-fill"
-              viewBox="0 0 16 16"
-            >
-              <path d="M16 14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12zm-4.5-6.5H5.707l2.147-2.146a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708-.708L5.707 8.5H11.5a.5.5 0 0 0 0-1z" />
-            </svg>
-          </NavLink>
-        </div>
         <NavLink to="/">
-          <img
-            src="https://surveymonkey-assets.s3.amazonaws.com/survey/182409455/e1ca79ba-8544-401a-b369-7cd97429a630.png"
-            style={{ height: 60, width: 100 }}
-            alt="Login"
-          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="50"
+            fill="ghostwhite"
+            class="bi bi-arrow-left-square-fill"
+            viewBox="0 0 16 16"
+          >
+          <path d="M16 14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12zm-4.5-6.5H5.707l2.147-2.146a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708-.708L5.707 8.5H11.5a.5.5 0 0 0 0-1z" />
+          </svg>
         </NavLink>
+        <NavLink to="/">
+          <div style={{paddingRight: 50}}>
+            <img src="https://surveymonkey-assets.s3.amazonaws.com/survey/182409455/e1ca79ba-8544-401a-b369-7cd97429a630.png" style={{ height: 60, width: 100}} alt="Login" />
+          </div>
+        </NavLink>
+        <div>
+                                   
+        </div>
       </div>
       <div className="containerChat">
         {/*<nav className="navbarTop">
